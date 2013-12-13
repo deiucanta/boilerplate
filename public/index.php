6d809ccc96837f28160a82d390a5b7786c9263ec
Andrei Canta <andrei.canta@deiu.ro>
2013-12-13 09:47:57 +0200

    Move /bootstrap into /backend

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../backend/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/start.php';
+$app = require_once __DIR__.'/../backend/bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
--- server.php
+++ server.php
@@ -4,7 +4,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = urldecode($uri);
 
-$paths = require __DIR__.'/bootstrap/paths.php';
+$paths = require __DIR__.'/backend/bootstrap/paths.php';
 
 $requested = $paths['public'].$uri;
 
